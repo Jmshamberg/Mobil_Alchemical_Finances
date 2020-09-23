e02f1265e9db2d14f3522a3112aee4972791ff4d
Jmshamberg <71047750+Jmshamberg@users.noreply.github.com>
2020-09-22 21:24:13 -0400

    Add files via upload

--- Homescreen.py
+++ Homescreen.py
@@ -1,5 +1,4 @@
 import os
-import kivy
 
 from kivy.app import App
 
@@ -10,6 +9,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+
 from kivy.graphics import Rectangle, Color
 from kivy.metrics import dp, sp
 from kivy.properties import ListProperty
@@ -20,22 +20,34 @@ class SummaryGrid(GridLayout):
         super(SummaryGrid, self).__init__(**kwargs)
         self.cols = 2
 
-        self.col1_width = dp(225)
+        self.window_size = Window.size
+        print(self.window_size)
+
+        self.col1_width = self.window_size[0] - dp(150)
         self.col2_width = dp(150)
 
         for x in range(0, 100, 1):
             accountx = Label(text=f'Target Retirement 2055',
                              size_hint_x=None,
                              size_hint_y=None,
-                             width=self.col1_width,
                              height=dp(35),
+                             width=self.col1_width,
                              color='#748b75')
 
             balancex = Label(text='$100,000.00',
-                             size_hint_x=None,
-                             size_hint_y=None,
-                             width=self.col2_width,
+                             size_hint_max=(self.col2_width, None),
                              height=dp(35))
+            # accountx = Label(text=f'Target Retirement 2055',
+            #                  size_hint_max=(self.col1_width, None),
+            #                  height=dp(35),
+            #                  color='#748b75',
+            #                  halign='right')
+            #
+            # balancex = Label(text='$100,000.00',
+            #                  size_hint_x=None,
+            #                  size_hint_y=None,
+            #                  width=self.col2_width,
+            #                  height=dp(35))
 
             self.add_widget(accountx)
             self.add_widget(balancex)
@@ -52,7 +64,7 @@ class HomeScreenApp(App):
         super(HomeScreenApp, self).__init__()
 
     def build(self):
-        Window.size = (dp(375), dp(775))
+        Window.size = (400, 775)
         root_widget = Interface()
         return root_widget
 
